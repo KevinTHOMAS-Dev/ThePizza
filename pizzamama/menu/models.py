35a576e1d6b17bfeafb09dc4e70b260252e14e65
KevinTHOMAS-Dev <128711174+KevinTHOMAS-Dev@users.noreply.github.com>
2024-02-22 21:28:41 +0100

    init

---
+++ pizzamama/menu/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+# Create your models here.
+
+
+class Pizza(models.Model):
+    name = models.CharField(max_length=200)
+    ingredients = models.CharField(max_length=400)
+    price = models.FloatField(default=0)
+    Vegetarienne = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
