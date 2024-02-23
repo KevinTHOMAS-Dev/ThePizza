e138295a710b10872de156c4871d9de48aa98852
KevinTHOMAS-Dev <128711174+KevinTHOMAS-Dev@users.noreply.github.com>
2024-02-23 19:09:42 +0100

    final project v2

--- pizzamama/menu/models.py
+++ pizzamama/menu/models.py
@@ -5,6 +5,7 @@ from django.db import models
 
 
 class Pizza(models.Model):
+    objects = None
     name = models.CharField(max_length=200)
     ingredients = models.CharField(max_length=400)
     price = models.FloatField(default=0)
--- pizzamama/menu/views.py
+++ pizzamama/menu/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 from .models import Pizza
-
+from django.core import serializers
+from django.http import HttpResponse
 
 
 # Create your views here.
 
 def index(request):
-    pizzas= Pizza.objects.all()
+    pizzas = Pizza.objects.all()
 
     return render(request,"menu/index.html", {"pizzas": pizzas})
 
+def api_get_pizzas(request):
+    pizzas = Pizza.objects.all()
+    json = serializers.serialize("json", pizzas)
+    return HttpResponse(json)
